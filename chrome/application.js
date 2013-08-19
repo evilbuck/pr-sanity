e505315150a24b06b2ed98fda342011e66a2adfb
Buckley Robinson <brobinson@verticalresponse.com>
2013-08-19 11:38:04 -0700

    - moved to jquery promises instead of ajax callbacks - now displaying files changed in the status - fixed a few errors - quick link to sort by oldest

--- chrome/application.js
+++ chrome/application.js
@@ -1,24 +1,68 @@
 ;(function($){
-
+  var $sort, $sortByOldestLink;
   // are we sorted by oldest?
   $sort = $('.issues-list-options .js-select-button');
   if (!(/oldest/i).test($sort.text())) {
-    $sort.parents('.select-menu').append('<span class="failed">&larr;This is not sorted by oldest?</span>');
+    $sortByOldestLink = $('<span class="failed">&larr;This is not sorted by oldest?</a></span>').click(function(){
+      window.location.href = window.location.origin + window.location.pathname + "?direction=asc&sort=created&state=open";
+    }).css('cursor', 'pointer');
+    $sort.parents('.select-menu').append($sortByOldestLink);
+  }
+
+  function updatePR(details) {
+    var $prListItem, $prHeader;
+
+    // save details
+    localStorage.setItem('pr:' + details.id, JSON.stringify(details));
+    $prListItem = this;
+    $prHeader = $prListItem.find('h4');
+
+    if ((/good to merge/).test(details.status)) {
+      $prListItem.addClass('passed');
+      status_color = 'green';
+    } else if ((/failed/).test(details.status)) {
+      $prListItem.addClass('failed');
+      status_color = 'red';
+    } else {
+      status_color = 'auto';
+    }
+    $prHeader.find('.pr-sanity').remove();
+    $prHeader.append('<div class="pr-sanity">' + 
+                       '<span style="font-weight: bold; color: blue">' + details.assignee + '-- <span class="files-changed">Files Changed: ' + details.files_changed + '</span></span>' +
+                       '<div style="clear: both; color: ' + status_color + '; font-size: 12px; font-weight: normal">' + status + '</div>' +
+                       '' +
+                       '<span class="updating" style="display:none">updating</span>' +
+                     '</div>');
   }
 
   $('.list-group-item h4 a').each(function(){
-    var $prHeader, $prListItem;
+    var $prHeader, $prListItem, $this, key, jqxhr, prNumber, cache;
 
     $prHeader = $(this).parent();
     $prListItem = $prHeader.parent();
+    $this = $(this);
 
-    $.ajax({
+    // did we already fetch this?
+    prNumber = $(this).attr('href').match(/\/([\d]+)$/)[1];
+    cache = localStorage.getItem('pr:' + prNumber);
+    $prListItem.find('.pr-sanity .updating').show();
+
+    if (cache) {
+      updatePR.call($prListItem, JSON.parse(cache));
+    }
+
+    jqxhr = $.ajax({
       url: $(this).attr('href'),
-      error: function(j, status, e){
-        console.warn('some error', e, status);
-      },
-      success: function(res){
-        var d, $prDoc, assignee, status, status_color;
+      dataType: 'html'
+    });
+
+
+    jqxhr.error(function(j, status, e){
+      console.warn('some error', e, status);
+    });
+
+    jqxhr.done(function(res){
+        var d, $prDoc, assignee, status, status_color, files_changed;
         // parse document
         d = document.implementation.createHTMLDocument('');
         d.body.innerHTML = res.replace(/<!DOCTYPE html>/i, '');
@@ -29,17 +73,11 @@
 
         // get status
         status = $prDoc.find('.merge-branch .branch-status').text().trim();
-        if ((/good to merge/).test(status)) {
-          $prListItem.addClass('passed');
-          status_color = 'green';
-        } else if ((/failed/).test(status)) {
-          $prListItem.addClass('failed');
-          status_color = 'red';
-        }
-        $prHeader.find('#pr-sanity').remove();
-        $prHeader.append('<div id="pr-sanity"><span style="font-weight: bold; color: blue">' + assignee + '</span><div style="clear: both; color: ' + status_color + '; font-size: 12px; font-weight: normal">' + status + '</div></div>');
-      },
-      dataType: 'html'
+
+        // get the files changed
+        files_changed = $prDoc.find('a[data-container-id="files_bucket"]').text().trim().replace(/[^\d]/gm, '');
+
+        updatePR.call($prListItem, {assignee: assignee, status: status, id: prNumber, files_changed: files_changed});
     });
-  })
+  });
 })(jQuery);
