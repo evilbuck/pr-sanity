86ec617d6ae2e1bbb5fde4cdf0f863cebf5fb1d9
Buckley Robinson <buckley@brightbytes.net>
2013-12-08 21:10:26 -0800

    Switched to a grunt build system
    - compressing build after copying
    - added cleanup
    - removed ruby dependence

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+var wrench = require('wrench');
+
+module.exports = function(grunt){
+  grunt.loadNpmTasks('grunt-contrib-compress');
+
+  grunt.registerTask('build', function(){
+    grunt.log.writeln('building Github PR Sanity');
+
+    wrench.copyDirSyncRecursive('./chrome', './pr-sanity', {
+      forceDelete: true,
+      preserveFiles: false
+    });
+    grunt.log.writeln('Compressing build');
+    grunt.task.run('compress');
+
+    grunt.log.writeln('cleaning up');
+    wrench.rmdirSyncRecursive('./pr-sanity');
+  });
+
+  grunt.initConfig({
+    compress: {
+      main: {
+        options: {
+          archive: 'pr-sanity.zip'
+        },
+        files: [
+          {src: ['pr-sanity/**'], dest: './'}
+        ]
+      }
+    }
+  });
+}
